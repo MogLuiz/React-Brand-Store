8bc8fbcaf472a9e2abd133f21a2c2aa68735a2b7
Luiz Henrique <luizhjramos@outlook.com>
2022-05-16 20:32:20 -0300

    chore(jestConfig): increasing collect coverage

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   testEnvironment: "jsdom",
   testPathIgnorePatterns: ["/node_modules/", "/.next/"],
   collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.ts(x)"],
+  collectCoverageFrom: ["<rootDir>/components/**/**/*.ts(x)", "<rootDir>/pages/*.ts(x)"],
   setupFilesAfterEnv: ["<rootDir>/.jest/setup.ts"],
 };
