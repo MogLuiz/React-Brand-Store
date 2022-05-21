cd6cfbd4e680a0c2631d383bd0fd1ca94a7038d9
Luiz Henrique <luizhjramos@outlook.com>
2022-05-21 16:35:15 -0300

    test(ProductList): should render the ProductCard component 10 times

--- pages-tests/product-list.integration.spec.tsx
+++ pages-tests/product-list.integration.spec.tsx
@@ -1,13 +1,25 @@
 // Packages
-import { screen, render } from "@testing-library/react";
+import { screen, render, waitFor } from "@testing-library/react";
 
 // Page
 import ProductList from "../pages";
 
+const setup = () => {
+  render(<ProductList />);
+};
+
 describe("ProductList", () => {
   it("should render ProductList", () => {
-    render(<ProductList />);
+    setup();
 
     expect(screen.getByTestId("product-list")).toBeInTheDocument();
+  });
+
+  fit("should render the ProductCard component 10 times", async () => {
+    setup();
+
+    await waitFor(() => {
+      expect(screen.getAllByTestId("product-card")).toHaveLength(10);
+    });
   });
 });
