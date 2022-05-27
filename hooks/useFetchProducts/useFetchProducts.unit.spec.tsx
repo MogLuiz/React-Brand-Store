c546c45db73e5ae11f7de5c7bd0f8de7dc0144b0
Luiz Henrique <luizhjramos@outlook.com>
2022-05-26 22:10:18 -0300

    test(useFetchProducts): should set error to true when catch() block is executed

--- hooks/useFetchProducts/useFetchProducts.unit.spec.tsx
+++ hooks/useFetchProducts/useFetchProducts.unit.spec.tsx
@@ -1,9 +1,13 @@
+// Packages
 import { screen, render } from "@testing-library/react";
-import { renderHook, act } from '@testing-library/react-hooks'
+import { renderHook, act } from "@testing-library/react-hooks";
 
+// Components
 import useFetchProducts from ".";
 
+// Services
 import { makeServer, TServer } from "../../services/mirage/server";
+import { Response } from "miragejs";
 
 describe("useFetchProducts", () => {
   let server: TServer;
@@ -19,11 +23,22 @@ describe("useFetchProducts", () => {
   it("should return a list of 10 products", async () => {
     server.createList("product", 10);
 
-    const { result, waitForNextUpdate } = renderHook(() => useFetchProducts())
+    const { result, waitForNextUpdate } = renderHook(() => useFetchProducts());
 
-    await waitForNextUpdate()
+    await waitForNextUpdate();
 
     expect(result.current.products).toHaveLength(10);
     expect(result.current.error).toBe(false);
+  });
+
+  it("should set error to true when catch() block is executed", async () => {
+    server.get("products", () => new Response(500, {}, ""));
+
+    const { result, waitForNextUpdate } = renderHook(() => useFetchProducts());
+
+    await waitForNextUpdate();
+
+    expect(result.current.error).toBe(true);
+    expect(result.current.products).toHaveLength(0);
   });
 });
