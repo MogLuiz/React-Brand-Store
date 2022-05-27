e44b1daa932726e4b6b7e1da9d3591e7d094d00a
Luiz Henrique <luizhjramos@outlook.com>
2022-05-26 22:52:58 -0300

    feat(ProductList): abstracting product rendering logic out of JSX

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,24 @@ const Home: NextPage = () => {
   // Custom Hooks
   // -------------------------------------------------
 
-  const { products, error } = useFetchProducts()
+  const { products, error } = useFetchProducts();
+
+  // -------------------------------------------------
+  // Functions
+  // -------------------------------------------------
+
+  const renderProducts = () => {
+    if (products.length === 0)
+      return <h4 aria-label="Empty products message">No products</h4>;
+
+    return products.map((product: any) => (
+      <ProductCard
+        product={product}
+        key={product.id}
+        addToCart={() => console.log("ola")}
+      />
+    ));
+  };
 
   // -------------------------------------------------
   // Render
@@ -26,16 +43,7 @@ const Home: NextPage = () => {
         <h3 className="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
         <span className="mt-3 text-sm text-gray-500">200+ Products</span>
         <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
-          {products.length === 0 && (
-            <h4 aria-label="Empty products message">No products</h4>
-          )}
-          {products.map((product: any) => (
-            <ProductCard
-              product={product}
-              key={product.id}
-              addToCart={() => console.log("ola")}
-            />
-          ))}
+          {renderProducts()}
         </div>
       </div>
     </main>
