9775716780b74258ae16e161c5fa9a4749a94c72
Luiz Henrique <luizhjramos@outlook.com>
2022-05-26 21:52:38 -0300

    test(useFetchProducts): testing the return of products from the request made in the hook with testing library react hooks

--- hooks/useFetchProducts/useFetchProducts.unit.spec.tsx
+++ hooks/useFetchProducts/useFetchProducts.unit.spec.tsx
@@ -1,4 +1,5 @@
 import { screen, render } from "@testing-library/react";
+import { renderHook, act } from '@testing-library/react-hooks'
 
 import useFetchProducts from ".";
 
@@ -15,11 +16,14 @@ describe("useFetchProducts", () => {
     server.shutdown();
   });
 
-  it("should return a list of 10 products", () => {
+  it("should return a list of 10 products", async () => {
     server.createList("product", 10);
 
-    const { products } = useFetchProducts();
+    const { result, waitForNextUpdate } = renderHook(() => useFetchProducts())
 
-    expect(products).toHaveLength(10);
+    await waitForNextUpdate()
+
+    expect(result.current.products).toHaveLength(10);
+    expect(result.current.error).toBe(false);
   });
 });
