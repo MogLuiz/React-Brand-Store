68ea6b85c979e65ce6867e6c20cc17a63945f0fb
Luiz Henrique <luizhjramos@outlook.com>
2022-05-28 17:46:53 -0300

    feat(ProductList): singular total products render implementarion

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ const Home: NextPage = () => {
     } else {
       setLocalProducts(
         products.filter(({ title }) => {
-          return title.toLowerCase().includes(term.toLowerCase())
+          return title.toLowerCase().includes(term.toLowerCase());
         })
       );
     }
@@ -54,6 +54,12 @@ const Home: NextPage = () => {
     return <h4>Server is down</h4>;
   };
 
+  const renderProductsQuantity = () => {
+    return localProducts.length === 1
+      ? `1 Product`
+      : `${localProducts.length} Products`;
+  };
+
   // -------------------------------------------------
   // Render
   // -------------------------------------------------
@@ -62,7 +68,9 @@ const Home: NextPage = () => {
       <Search doSearch={(term) => setTerm(term)} />
       <div className="container mx-auto px-6">
         <h3 className="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
-        <span className="mt-3 text-sm text-gray-500">{localProducts.length} Products</span>
+        <span className="mt-3 text-sm text-gray-500">
+          {renderProductsQuantity()}
+        </span>
         <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
           {renderErrorMessage()}
           {renderProducts()}
