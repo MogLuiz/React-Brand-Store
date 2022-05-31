1115a2bb2b5fd5e73364884390e8b8284582de13
Luiz Henrique <luizhjramos@outlook.com>
2022-05-30 22:13:56 -0300

    test(store): should return an empty array for products on initial state

--- store/CartStore.unit.spec.tsx
+++ store/CartStore.unit.spec.tsx
@@ -2,13 +2,19 @@ import { renderHook, act } from "@testing-library/react-hooks";
 import useCartStore from "./Cart";
 
 describe("Cart Store", () => {
-  it("should return open equals false on initial state", async () => {
+  it("should return open equals false on initial state", () => {
     const { result } = renderHook(() => useCartStore());
 
     expect(result.current.state.open).toBe(false);
   });
+  it("should return an empty array for products on initial state", () => {
+    const { result } = renderHook(() => useCartStore());
 
-  it("should toggle open state", async () => {
+    expect(typeof result.current.state.products).toBe('array')
+    expect(result.current.state.products).toHaveLength(0)
+  });
+
+  it("should toggle open state", () => {
     const { result } = renderHook(() => useCartStore());
     const {
       actions: { toggle },
