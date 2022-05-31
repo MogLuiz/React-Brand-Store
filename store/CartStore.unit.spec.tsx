70ab229a1c2ce3207b646c344808e9e0ff006132
Luiz Henrique <luizhjramos@outlook.com>
2022-05-30 22:02:15 -0300

    test(store): should toggle open state

--- store/CartStore.unit.spec.tsx
+++ store/CartStore.unit.spec.tsx
@@ -1,9 +1,24 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook, act } from "@testing-library/react-hooks";
 import useCartStore from "./Cart";
 
 describe("Cart Store", () => {
   it("should return open equals false on initial state", async () => {
     const { result } = renderHook(() => useCartStore());
+
+    expect(result.current.state.open).toBe(false);
+  });
+
+  it("should toggle open state", async () => {
+    const { result } = renderHook(() => useCartStore());
+    const {
+      actions: { toggle },
+    } = result.current;
+
+    expect(result.current.state.open).toBe(false);
+    act(() => toggle());
+
+    expect(result.current.state.open).toBe(true);
+    act(() => toggle());
 
     expect(result.current.state.open).toBe(false);
   });
