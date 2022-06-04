53692b8b936119fed29b8bc000b4ccf431ad8a54
Luiz Henrique <luizhjramos@outlook.com>
2022-06-04 18:15:36 -0300

    test(Cart): mount before setup test

---
+++ components/Cart/cart.integration.spec.tsx
@@ -0,0 +1,36 @@
+// Packages
+import { render, screen } from "@testing-library/react";
+import { renderHook, act, RenderResult } from "@testing-library/react-hooks";
+import userEvent from "@testing-library/user-event";
+
+// Components
+import Cart from ".";
+
+// Store
+import useCartStore, { IUseCartState, ProductType } from "../../store/Cart";
+
+// Services
+import { makeServer, TServer } from "../../services/mirage/server";
+
+describe("Cart", () => {
+  let server: TServer;
+  let result: RenderResult<IUseCartState>;
+  let addProduct: <T extends ProductType>(product: T | ProductType) => void;
+  let toggle: () => void;
+  let spy;
+  let reset: () => void;
+
+  beforeEach(() => {
+    server = makeServer({ environment: "test" });
+    result = renderHook(() => useCartStore()).result;
+    addProduct = result.current.actions.addProduct;
+    reset = result.current.actions.reset;
+    toggle = result.current.actions.toggle;
+    spy = jest.spyOn(result.current.actions, "toggle");
+  });
+
+  afterEach(() => {
+    server.shutdown();
+    jest.clearAllMocks();
+  });
+});
--- components/Cart/index.tsx
+++ components/Cart/index.tsx
@@ -16,6 +16,7 @@ const Cart: React.FC = () => {
   // -------------------------------------------------
   return (
     <div
+      data-testid="cart"
       className={`${
         !open && "hidden"
       } fixed right-0 top-0 max-w-xs w-full h-full px-6 py-4 transition duration-300 transform overflow-y-auto bg-white border-l-2 border-gray-300`}
